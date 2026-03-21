dbc23fbe5cc38a56718cff3ffea60fc8abbcb996
Fatema Althawadi <codebyfatem@gmail.com>
2026-03-22 01:23:28 +0300

    UI enhanced + grid supports up to 100x100 grid

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let input = prompt("Enter the size of the grid (1-64): ");
+let input = prompt("Enter the size of the grid (1-100): ");
 const grid = document.querySelector(".grid");
 
 let size = Number(input);
 
-if (size > 64 || size < 1) {
+if (size > 100 || size < 1) {
 	const error = document.createElement("div");
 	error.textContent =
 		"Cannot display grid, pixel size not supported, please try again.";
@@ -15,7 +15,7 @@ if (size > 64 || size < 1) {
 
 function generateGrid(size) {
     grid.innerHTML='';
-    let pxSize = 946/(size);
+    let pxSize = 646/size;
     for(let row = 1; row <= size; row++){
       for (let column=1; column<=size; column++){
         const pixel = document.createElement('div');
